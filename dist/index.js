b0741c1e6e116c2561efc0a26472f78656a8388d
khatab AL-DAGHISTANI <khatobi79@users.noreply.github.co>
2020-09-14 19:43:25 -0700

    config

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
-console.log("heloo world");
+function helloWorld(message) {
+    console.log("Hello World");
+    var updatedMessage = { f: 1, b: 2 };
+    return 0;
+}
+helloWorld("goodbye world");
 //# sourceMappingURL=index.js.map
